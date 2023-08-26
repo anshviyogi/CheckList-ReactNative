038b7b687153e0e2abee758598aed7099242ce68
ansh.viyogi <ansh.viyogi@inadev.com>
2023-08-26 16:11:16 +0530

    crud operation react native

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  Button,
+  ScrollView,
+} from "react-native";
 
-export default function App() {
+import { TailwindProvider } from "tailwindcss-react-native";
+import uuid from "react-native-uuid";
+
+function App() {
+  const [message, setMessage] = useState(null);
+  const [list, setList] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [editItem, setEditItem] = useState(null);
+
+  function saveHandler() {
+    if (!message) {
+      return alert("Please type something");
+    }
+
+    const obj = {
+      id: uuid.v4(),
+      message,
+    };
+    setList([...list, obj]);
+    setMessage("");
+  }
+
+  function deleteHandler(id) {
+    let itemSort = list.filter((item) => item.id !== id);
+    setList(itemSort);
+  }
+
+  function editHandler() {
+    let editItemKey = list.map((item) => {
+      if (item.id === editItem.id) {
+        item.message = message;
+        setMessage("");
+        return setIsEdit(false);
+      }
+    });
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TailwindProvider>
+      <ScrollView>
+        <SafeAreaView style={{ flex: 1 }}>
+          <View className="mt-10">
+            {/* INPUT FIELD AND ACTION BUTTON */}
+            <View className="flex flex-row">
+              <TextInput
+                className="border border-black w-[85%]"
+                placeholder="Go to Gym"
+                value={message}
+                onChangeText={(e) => {
+                  setMessage(e);
+                }}
+              />
+              {isEdit ? (
+                <Button title="Update" onPress={editHandler} />
+              ) : (
+                <Button title="Save" onPress={saveHandler} />
+              )}
+            </View>
+
+            {/* List to be displayed*/}
+            <View>
+              {list?.map((item) => (
+                <View className="flex-row justify-between px-5 py-2">
+                  <Text>{item?.message}</Text>
+                  <View className="flex-row">
+                    <Button
+                      title="Edit"
+                      onPress={() => {
+                        setIsEdit(true);
+                        setMessage(item.message);
+                        setEditItem(item);
+                      }}
+                    />
+                    <Button
+                      title="Delete"
+                      onPress={() => deleteHandler(item.id)}
+                      color={"red"}
+                    />
+                  </View>
+                </View>
+              ))}
+            </View>
+          </View>
+        </SafeAreaView>
+      </ScrollView>
+    </TailwindProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
